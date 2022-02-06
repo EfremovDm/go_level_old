1caf11d190b4e46f409ab28b1d5ac0fa6f23cfc9
Dima <dima@dima.ru>
2022-02-06 18:03:29 +0300

    Урок 1. История возникновения языка Go. Особенности языка Go. Работа с плейграундом и основы тулчейна

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+/**
+ * Урок 1. История возникновения языка Go. Особенности языка Go.
+ * Работа с плейграундом и основы тулчейна
+ */
+func main() {
+
+	var hello string = "Hello, world!"
+	println(hello)
+
+	// go run main.go
+
+	// go build
+	var input string
+	fmt.Scanf("%v",&input)
+}
